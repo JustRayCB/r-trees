28f2ea612a6bea9f0fee93ac22422a789ee8289e
JustRayCB <rayancbpro@gmail.com>
2023-04-13 14:15:27 +0200

    Mise en évidence des choses importantes

--- src/main/java/projet/SinglePoint.java
+++ src/main/java/projet/SinglePoint.java
@@ -1,43 +1,34 @@
 /*
  *    Annexe pour l'énoncé du projet d'INFOF203 (2022-2023)
- *    
+ *
  */
 
-//package be.ulb.infof203.projet;
+// package be.ulb.infof203.projet;
 package projet;
 
 import java.awt.Color;
 import java.io.File;
-
+import java.util.Random;
 import org.geotools.data.FileDataStore;
 import org.geotools.data.FileDataStoreFinder;
-
-import org.geotools.data.simple.SimpleFeatureSource;
 import org.geotools.data.collection.ListFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.feature.simple.SimpleFeatureBuilder;
+import org.geotools.geometry.jts.GeometryBuilder;
+import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.map.FeatureLayer;
 import org.geotools.map.Layer;
 import org.geotools.map.MapContent;
 import org.geotools.styling.SLD;
 import org.geotools.styling.Style;
-
+import org.geotools.swing.JMapFrame;
 import org.locationtech.jts.geom.MultiPolygon;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.Polygon;
-import org.geotools.geometry.jts.ReferencedEnvelope;
-
-
-import org.geotools.geometry.jts.GeometryBuilder;
-
-import org.geotools.data.simple.SimpleFeatureCollection;
-import org.geotools.feature.simple.SimpleFeatureBuilder;
-import org.geotools.data.simple.SimpleFeatureIterator;
 import org.opengis.feature.Property;
 import org.opengis.feature.simple.SimpleFeature;
-
-import java.util.Random;
-
-import org.geotools.swing.JMapFrame;
-
 
 /**
  * Prompts the user for a shapefile and displays the contents on the screen in a map frame.
@@ -46,23 +37,23 @@ import org.geotools.swing.JMapFrame;
  */
 public class SinglePoint {
 
-
     /**
      * GeoTools Quickstart demo application. Prompts the user for a shapefile and displays its
      * contents on the screen in a map frame
      */
     public static void main(String[] args) throws Exception {
         // display a data store file chooser dialog for shapefiles
-        //String filename="../projetinfof203/data/sh_statbel_statistical_sectors_31370_20220101.shp/sh_statbel_statistical_sectors_31370_20220101.shp";
+        // String
+        // filename="../projetinfof203/data/sh_statbel_statistical_sectors_31370_20220101.shp/sh_statbel_statistical_sectors_31370_20220101.shp";
 
-        //String filename="../projetinfof203/data/WB_countries_Admin0_10m/WB_countries_Admin0_10m.shp";
-        //String filename="/home/cbr/50m_cultural/WB_countries_Admin0_10m.shp";
-        //String filename="/home/cbr/Unif2/50m_cultural/WB_countries_Admin0_10m.shp";
-        String filename="/home/cbr/Unif2/WB_countries_Admin0_10m/WB_countries_Admin0_10m.shp";
+        // String
+        // filename="../projetinfof203/data/WB_countries_Admin0_10m/WB_countries_Admin0_10m.shp";
+        // String filename="/home/cbr/50m_cultural/WB_countries_Admin0_10m.shp";
+        // String filename="/home/cbr/Unif2/50m_cultural/WB_countries_Admin0_10m.shp";
+        String filename = "/home/cbr/Unif2/WB_countries_Admin0_10m/WB_countries_Admin0_10m.shp";
 
-        //String filename="../projetinfof203/data/communes-20220101-shp/communes-20220101.shp";
-        //String filename="/home/cbr/50m_cultural/ne_10m_admin_1_sel.shp";
-
+        // String filename="../projetinfof203/data/communes-20220101-shp/communes-20220101.shp";
+        // String filename="/home/cbr/50m_cultural/ne_10m_admin_1_sel.shp";
 
         File file = new File(filename);
         if (!file.exists())
@@ -71,40 +62,38 @@ public class SinglePoint {
         FileDataStore store = FileDataStoreFinder.getDataStore(file);
         SimpleFeatureSource featureSource = store.getFeatureSource();
 
-        SimpleFeatureCollection all_features=featureSource.getFeatures();
+        SimpleFeatureCollection all_features = featureSource.getFeatures();
 
         store.dispose();
 
         ReferencedEnvelope global_bounds = featureSource.getBounds();
 
-
         Random r = new Random();
 
-
         GeometryBuilder gb = new GeometryBuilder();
-        //Point p = gb.point(152183, 167679);// Plaine
-        //Point p = gb.point(4.4, 50.8);// 
-        //Point p = gb.point(58.0, 47.0);
-        //Point p = gb.point(10.6,59.9);// Oslo
+        // Point p = gb.point(152183, 167679);// Plaine
+        // Point p = gb.point(4.4, 50.8);//
+        // Point p = gb.point(58.0, 47.0);
+        // Point p = gb.point(10.6,59.9);// Oslo
 
-        Point p = gb.point(-70.9,-33.4);// Santiago
-                                        //Point p = gb.point(169.2, -52.5);//NZ
+        Point p = gb.point(-70.9, -33.4); // Santiago
+                                          // Point p = gb.point(169.2, -52.5);//NZ
 
-                                        //Point p = gb.point(172.97365198326708, 1.8869725782923172);
+        // Point p = gb.point(172.97365198326708, 1.8869725782923172);
 
+        // Point p = gb.point(r.nextInt((int) global_bounds.getMinX(), (int)
+        // global_bounds.getMaxX()), r.nextInt((int) global_bounds.getMinY(), (int)
+        // global_bounds.getMaxY()));
 
-                                        //Point p = gb.point(r.nextInt((int) global_bounds.getMinX(), (int) global_bounds.getMaxX()),
-                                        //r.nextInt((int) global_bounds.getMinY(), (int) global_bounds.getMaxY()));
+        SimpleFeature target = null;
 
-        SimpleFeature target=null;
+        System.out.println(all_features.size() + " features");
 
-        System.out.println(all_features.size()+" features");
-
-        try ( SimpleFeatureIterator iterator = all_features.features() ){
-            while( iterator.hasNext()){
+        try (SimpleFeatureIterator iterator = all_features.features()) {
+            while (iterator.hasNext()) {
                 SimpleFeature feature = iterator.next();
 
-                MultiPolygon polygon = (MultiPolygon) feature.getDefaultGeometry();
+                MultiPolygon polygon = (MultiPolygon)feature.getDefaultGeometry();
 
                 if (polygon != null && polygon.contains(p)) {
                     target = feature;
@@ -117,9 +106,9 @@ public class SinglePoint {
             System.out.println("Point not in any polygon!");
 
         else {
-            for(Property prop: target.getProperties()) {
+            for (Property prop : target.getProperties()) {
                 if (prop.getName().toString() != "the_geom") {
-                    System.out.println(prop.getName()+": "+prop.getValue());
+                    System.out.println(prop.getName() + ": " + prop.getValue());
                 }
             }
         }
@@ -132,26 +121,22 @@ public class SinglePoint {
         map.addLayer(layer);
 
         ListFeatureCollection collection = new ListFeatureCollection(featureSource.getSchema());
-        SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(featureSource.getSchema()); 
-
+        SimpleFeatureBuilder featureBuilder = new SimpleFeatureBuilder(featureSource.getSchema());
 
         // Add target polygon
         collection.add(target);
 
         // Add Point
-        Polygon c= gb.circle(p.getX(), p.getY(), all_features.getBounds().getWidth()/200,10);
+        Polygon c = gb.circle(p.getX(), p.getY(), all_features.getBounds().getWidth() / 200, 10);
         featureBuilder.add(c);
         collection.add(featureBuilder.buildFeature(null));
 
         // Add MBR
-        if (target !=null) {
-            featureBuilder.add(gb.box(target.getBounds().getMinX(),
-                        target.getBounds().getMinY(),
-                        target.getBounds().getMaxX(),
-                        target.getBounds().getMaxY()
-                        ));
+        if (target != null) {
+            featureBuilder.add(gb.box(target.getBounds().getMinX(), target.getBounds().getMinY(),
+                                      target.getBounds().getMaxX(), target.getBounds().getMaxY()));
 
-            //collection.add(featureBuilder.buildFeature(null));
+            // collection.add(featureBuilder.buildFeature(null));
 
             collection.add(featureBuilder.buildFeature(null));
         }
